9f263388052c5a77485ea58d7b468d2f0110787f
Matthieu Stadelmann <stadelmannmatthieu@gmail.com>
2025-03-07 15:42:05 +0100

    Add extends to eslint

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,11 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import { ESLint } from "eslint"; // Import ESLint for correct usage
 import tsParser from "@typescript-eslint/parser";
 import tsPlugin from "@typescript-eslint/eslint-plugin";
 import prettier from "eslint-plugin-prettier";
 
-const config = {
+export default {
   ignores: ["dist"],
   extends: [
     "eslint:recommended",
@@ -40,7 +39,3 @@ const config = {
     "prettier/prettier": "error",
   },
 };
-
-const eslint = new ESLint({ baseConfig: config });
-
-export default eslint;
